21d20ba08decd2c7184272687fa16a963f09cedb
mrunelov <mrunelov@kth.se>
2015-03-30 15:52:02 +0200

    misc

--- graph-tool/AAN/find_fellows.py
+++ graph-tool/AAN/find_fellows.py
@@ -8,7 +8,7 @@ def parse_names(fullname, has_firstname=True, reverse=False):
     names[-1] = names[-1].strip() # remove newline
     if reverse: # first name is last, put it first
         r_i = len(names) - 1
-        while len(names[r_i]) <= 2: # move past e.g. "C." and such
+        while r_i >= 0 and len(names[r_i]) <= 2: # move past e.g. "C." and such
             r_i -= 1
         names = names[r_i:] + names[:r_i]
     for name in names:
@@ -34,30 +34,33 @@ def parse_fellows():
 
 
 fellows = parse_fellows()
-def is_fellow(candidate, has_firstname=True, reverse=False):
-    found_fellow = False
+def find_fellow(candidate, has_firstname=True, reverse=False):
+    fellow_index = -1
+    if not candidate:
+        return fellow_index
     name = parse_names(candidate,has_firstname=has_firstname, reverse=reverse)
     print "Checking fellow: " + str(name)
-    for fellow in fellows:
+    best_match = 0.0
+    for i,fellow in enumerate(fellows):
         s = sim(fellow[1],name[-1])
-        if s >= 0.7:
+        if s >= 0.7: 
             if has_firstname: # check first name if available
                 s2 = sim(fellow[0],name[0])
                 print "Checked first names: " + fellow[0] + ", " + name[0] + " with similarity " + str(s2)
                 if s2 < 0.7:
                     continue
-            found_fellow = True
+            if s > best_match:
+                fellow_index = i
+                best_match = s
             print "Found probable match:"
             print names_to_str(fellow) + " MATCHES " + names_to_str(name) 
-    if found_fellow:
-        return True
-    return False
+    return fellow_index 
+
 
 def sim(a, b):
     #print "COMPARING " + a + " AND " + b
     seq = difflib.SequenceMatcher(a=a.lower(), b=b.lower())
     return seq.ratio()
-
 
 #candidates = ["Kaplan", "Mercer", "Moore", "Tou Ng", "Palmer"]
 #for c in candidates:
