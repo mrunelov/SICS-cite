ab94a84d8c517d011ad6495e40e014e783020bcd
mrunelov <mrunelov@kth.se>
2015-03-30 16:47:36 +0200

    refactoring

--- graph-tool/AAN/betweenness.py
+++ graph-tool/AAN/betweenness.py
@@ -1,14 +1,16 @@
+import sys
 import graph_tool.all as gt
 import matplotlib
 import pickle
 import numpy as np
+from sets import Set
 from find_fellows import *
 
 """
 Calculates betweenness centrality for a co-citation network and prints info and plots the top results
 """
 
-num_top = 100
+num_top = 1000
 
 g = gt.load_graph("co-citation.graphml")
 g.set_directed(False)
@@ -28,7 +30,7 @@ print "Loaded a graph with " + str(g.num_vertices()) + " nodes"
 
 with open("vpa.pickle","rb") as f:
     vpa = np.asarray(pickle.load(f))
-top_vp = vpa.argsort()[::-1][:num_top]
+top_vp = vpa.argsort()[::-1]#[:num_top]
 
 #with open("vpa.pickle","wb") as f:
     #pickle.dump(betweens,f)
@@ -42,17 +44,17 @@ top_vp = vpa.argsort()[::-1][:num_top]
 # can't pickle vertex objects atm, but convert to int then save might work.
 # so, maybe try looping vp.a
 
-#bg = gt.GraphView(g, vfilt=lambda v: vp[v] > vp.a.max() / 4)
-top_nodes = [g.vertex(v) for v in top_vp]
-bg = gt.GraphView(g, vfilt=lambda v: v in top_nodes)
-print "Number of nodes after filtering: " + str(bg.num_vertices())
-
-
 i = 0
 fellow_articles = 0
-for i in range(num_top):
+fellows = Set(range(32))
+#for i in range(num_top):
 #for v in bg.vertices():
-    v = g.vertex(top_vp[i])
+total = len(top_vp)
+for i,v_i in enumerate(top_vp):
+    sys.stderr.write("looping node " + str(i+1) + " / " + str(total) + "\r")
+    sys.stderr.flush()
+    #v = g.vertex(top_vp[i])
+    v = g.vertex(v_i)
     print "##############"
     print "###   " + str(i+1) + "   ###"
     print "##############"
@@ -63,22 +65,31 @@ for i in range(num_top):
     auths = auths.split(";")
     found_fellow = False
     for a in auths:
-        if is_fellow(a,reverse=True):
-            found_fellow = True
-    if found_fellow:
-        fellow_articles += 1
-    print "##############"
+        fellow_index = find_fellow(a,reverse=True)
+        if fellow_index is not -1:
+            fellow_articles += 1
+            if fellow_index in fellows:
+                fellows.remove(fellow_index)
+    if len(fellows) == 1: # found all fellows except Tou Ng
+        break
+    print "###################################################"
     i += 1
     #if i > 10:
         #break
 print "Fellow articles: " + str(fellow_articles) + " / " + str(num_top)
+print "Fellows remaining: " + str(fellows)
 
-print "Calculating layout..." 
-pos = gt.fruchterman_reingold_layout(bg,n_iter=10) 
-print "Done calculating layout. Plotting."
+#bg = gt.GraphView(g, vfilt=lambda v: vp[v] > vp.a.max() / 4)
+#top_nodes = [g.vertex(v) for v in top_vp]
+#bg = gt.GraphView(g, vfilt=lambda v: v in top_nodes)
+#print "Number of nodes after filtering: " + str(bg.num_vertices())
 
-gt.graph_draw(bg, pos=pos, #vertex_fill_color=vpa,
-        vertex_size=gt.prop_to_size(in_degs, mi=7, ma=25),
-        #edge_pen_width=gt.prop_to_size(ep, mi=0.5, ma=5),
-        vcmap=matplotlib.cm.gist_heat,
-        output="co-citation_betweenness.pdf") #vorder=vpa, 
+#print "Calculating layout..." 
+#pos = gt.fruchterman_reingold_layout(bg,n_iter=10) 
+#print "Done calculating layout. Plotting."
+
+#gt.graph_draw(bg, pos=pos, #vertex_fill_color=vpa,
+        #vertex_size=gt.prop_to_size(in_degs, mi=7, ma=25),
+        ##edge_pen_width=gt.prop_to_size(ep, mi=0.5, ma=5),
+        #vcmap=matplotlib.cm.gist_heat,
+        #output="co-citation_betweenness.pdf") #vorder=vpa, 
