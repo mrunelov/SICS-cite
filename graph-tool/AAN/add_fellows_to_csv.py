f392184df8585bfccc4e9e305cf6c67d3069bbea
mrunelov <mrunelov@kth.se>
2015-04-05 12:22:58 +0200

    added support for new csv files with index

--- graph-tool/AAN/add_fellows_to_csv.py
+++ graph-tool/AAN/add_fellows_to_csv.py
@@ -1,7 +1,8 @@
 import graph_tool.all as gt
+from collections import defaultdict
 from find_fellows import *
 
-g = gt.load_graph("AAN-preprocessed.xml")
+g = gt.load_graph("AAN.graphml") #"AAN-preprocessed.xml")
 titles = g.vertex_properties["title"]
 authors = g.vertex_properties["authors"]
 
@@ -14,6 +15,7 @@ def get_authors_for_title(title):
 
 
 
+index_fellow_map = defaultdict(False)
 with open("all_AAN.csv","r") as old,\
     open("all_AAN_with_fellows.csv", "w+") as new:
     header = old.next()
@@ -21,7 +23,7 @@ with open("all_AAN.csv","r") as old,\
     for line in old:
         old_data = line.strip().split(",")
         title = old_data[0]
-
+        gt_index = old_data[1]
         author_list = get_authors_for_title(title).split(";")
         if author_list is None:
             print "Didn't find matching node for title:"
@@ -34,7 +36,12 @@ with open("all_AAN.csv","r") as old,\
                     break
             if fellow == -1:
                 fellow = 0
+            else:
+                index_fellow_map[gt_index] = True
             new_data = line.strip() + "," + str(fellow) + "\n"
             new.write(new_data)
+
+with open("index_fellow_map.pickle") as f:
+    pickle.dump(index_fellow_map,f)
 
 
