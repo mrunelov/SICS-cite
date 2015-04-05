bd77c7c4b374981f64873cedcfd4fd04ff062596
mrunelov <mrunelov@kth.se>
2015-04-06 00:35:23 +0200

    added tmp save mechanism for long running co_citation calculations

--- algorithms/co_citation.py
+++ algorithms/co_citation.py
@@ -6,7 +6,9 @@ dataset = conf.settings['dataset']
 
 G = gt.load_graph("../" + dataset + "/data/" + dataset + ".graphml") #"AAN-preprocessed.xml")
 
+print "Loaded a graph with " + str(G.num_vertices()) + " vertices and " + str(G.num_edges()) + " edges."
 CC = gt.Graph(G,directed=False)
+
 for edge in G.edges():
     CC.remove_edge(edge)
 
@@ -15,20 +17,23 @@ CC.edge_properties["weight"] = weight
 print G.list_properties()
 
 N = str(G.num_vertices())
-i = 1
+idx = 0
 for n in G.vertices():
-    i += 1
+    if idx%10001 == 0:
+        CC.save("tmp/co-citation-" + dataset + "-" + str(idx) + ".graphml")
     out_edges = list(n.out_edges())
-    print "Looping node " + str(i) + " / " + N + " with " + str(len(out_edges)) + " references" + "\r",
+    print "Looping node " + str(idx+1) + " / " + N + " with " + str(len(out_edges)) + " references" + "                \r",
     refs = [e.target() for e in out_edges]
-    for u,v in itertools.combinations(refs,2):
-        edge = CC.edge(u,v)
-        if edge is None:
-            edge = CC.add_edge(u,v)
-            weight[edge] = 0 # make sure weights start at 0
-        weight[edge] += 1
-        if weight[edge] == 0:
-            print "WTF?"
+    for i in range(len(refs)-1):
+    #for u,v in itertools.combinations(refs,2):
+        u = refs[i]
+        for v in refs[i+1:]:
+            edge = CC.edge(u,v)
+            if edge is None:
+                edge = CC.add_edge(u,v)
+                weight[edge] = 0 # make sure weights start at 0
+            weight[edge] += 1
+    idx += 1
 
 
 CC.save("co-citation-" + dataset + ".graphml")
