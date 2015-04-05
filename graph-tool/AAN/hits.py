1d0c0b65ec8a19d922ef961cbe6db9d28408a6ec
mrunelov <mrunelov@kth.se>
2015-04-05 12:03:29 +0200

    same as for indeg and between, AAN.graphml instead of sci2s

--- graph-tool/AAN/hits.py
+++ graph-tool/AAN/hits.py
@@ -5,7 +5,7 @@ import numpy as np
 
 num_top = 10
 
-g = gt.load_graph("AAN-preprocessed.xml")
+g = gt.load_graph("AAN.graphml") #"AAN-preprocessed.xml")
 #g.list_properties()
 # date exists in this map. see code in other AAN folder for inspiration
 # TODO: do HITS with different graph views. So, todo is: learn to filter on dates.
@@ -22,34 +22,40 @@ top_nodes = [g.vertex(v) for v in top_auths]
 bg = gt.GraphView(g, vfilt=lambda v: v in top_auths)
 
 with open("hits.csv", "w+") as csv:
-    for x in top_auths:
-        v = g.vertex(x)
-        line = titles[v].strip().replace(",","") + "," + str(auths[v]) + "\n"
+    csv.write("title,gt_index,hits_auth")
+    #for x in top_auths:
+    i = 0
+    for n in g.vertices():
+        #v = g.vertex(x)
+        line = titles[n].strip().replace(",","") +\
+                "," + str(i) + "," + str(auths[n]) + "\n"
+        i += 1
         csv.write(line)
+print "Wrote hits.csv"
 
-i = 0
-fellow_articles = 0
-for i in range(num_top):
-    v = g.vertex(top_auths[i])
-    print "################"
-    print "###   " + str(i+1) + "   ###"
-    print "################"
-    print titles[v]
-    print authors[v]
-    print "Authority score: " + str(auths[v])
-    author_str = authors[v]
-    author_list = author_str.split(";")
-    # TODO: old code. see betweenness.py for how it's done now
-    found_fellow = False
-    for a in author_list:
-        if is_fellow(a,reverse=True):
-            found_fellow = True
-    if found_fellow:
-        fellow_articles += 1
-    print "#################"
-    i += 1
+#i = 0
+#fellow_articles = 0
+#for i in range(num_top):
+    #v = g.vertex(top_auths[i])
+    #print "################"
+    #print "###   " + str(i+1) + "   ###"
+    #print "################"
+    #print titles[v]
+    #print authors[v]
+    #print "Authority score: " + str(auths[v])
+    #author_str = authors[v]
+    #author_list = author_str.split(";")
+    ## TODO: old code. see betweenness.py for how it's done now
+    #found_fellow = False
+    #for a in author_list:
+        #if is_fellow(a,reverse=True):
+            #found_fellow = True
+    #if found_fellow:
+        #fellow_articles += 1
+    #print "#################"
+    #i += 1
 
-print "Fellow articles: " + str(fellow_articles) + " / " + str(num_top)
+#print "Fellow articles: " + str(fellow_articles) + " / " + str(num_top)
 
 
 
