7b402de0a77e301a747758ea7cd390fade304e06
mrunelov <mrunelov@kth.se>
2015-04-07 15:04:03 +0200

    working co-citation, but untried with tmp files

--- algorithms/co_citation.py
+++ algorithms/co_citation.py
@@ -4,39 +4,49 @@ import config as conf
 
 dataset = conf.settings['dataset']
 
-#G = gt.load_graph("../" + dataset + "/data/" + dataset + ".graphml") #"AAN-preprocessed.xml")
+G = gt.load_graph("../" + dataset + "/data/" + dataset + ".graphml") #"AAN-preprocessed.xml")
+print "Loaded a graph with " + str(G.num_vertices()) + " vertices and " + str(G.num_edges()) + " edges."
 
-#print "Loaded a graph with " + str(G.num_vertices()) + " vertices and " + str(G.num_edges()) + " edges."
+del G.properties[("v","date")]
+del G.properties[("v","label")]
 
-CC = gt.load_graph("tmp/co-citation-APS-10000.graphml") #gt.Graph(G,directed=False)
-print "Loaded a graph with " + str(CC.num_vertices()) + " vertices and " + str(CC.num_edges()) + " edges."
+G2 = gt.GraphView(G, vfilt=lambda v: v.in_degree() > 0,
+        efilt=lambda e: e.source().out_degree >= 2)
+print "After filtering there are " + str(G2.num_vertices()) + " vertices and " + str(G2.num_edges()) + " edges."
+G2.purge_vertices()
 
-#for edge in G.edges():
-    #CC.remove_edge(edge)
+#CC = gt.Graph(G2) #gt.load_graph("tmp/co-citation-APS-10000.graphml") 
+#CC = gt.GraphView(CC,efilt=lambda e: False)
+#CC.purge_edges()
+#CC.set_directed(False)
+CC = gt.load_graph("tmp/co-citation-APS-20000.graphml") # load partly processed graph
 
-weight = CC.edge_properties["weight"] #CC.new_edge_property("int")
-#CC.edge_properties["weight"] = weight
-#print G.list_properties()
+weight = CC.new_edge_property("int") #CC.edge_properties["weight"] 
+CC.edge_properties["weight"] = weight
 
 N = str(CC.num_vertices())
-idx = 0
-for n in CC.vertices():
+idx = -1
+for n in G2.vertices():
     idx += 1
-    if idx <= 10000:
+    if idx <= 20000:
         continue
     if idx%10000 == 0:
         CC.save("tmp/co-citation-" + dataset + "-" + str(idx) + ".graphml")
     out_edges = list(n.out_edges())
+    if len(out_edges) <= 1:
+        continue
     print "Looping node " + str(idx+1) + " / " + N + " with " + str(len(out_edges)) + " references" + "                \r",
     refs = [e.target() for e in out_edges]
-    for i in range(len(refs)-1):
-    #for u,v in itertools.combinations(refs,2):
-        u = refs[i]
-        for v in refs[i+1:]:
-            edge = CC.edge(u,v)
-            if edge is None:
-                edge = CC.add_edge(u,v)
-                weight[edge] = 0 # make sure weights start at 0
-            weight[edge] += 1
+    #for i in range(len(refs)-1):
+    for u,v in itertools.combinations(refs,2):
+        #u = refs[i]
+        #for v in refs[i+1:]:
+        edge = CC.edge(u,v)
+        if edge is None:
+            edge = CC.add_edge(u,v)
+            weight[edge] = 0 # make sure weights start at 0
+        weight[edge] += 1
 
+print "Saving co-citation graph with " + str(CC.num_vertices()) + " nodes and " +\
+        str(CC.num_edges()) + " edges."
 CC.save("co-citation-" + dataset + ".graphml")
